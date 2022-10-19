b89525ada7cd927bf4853a2aba8738966e4d1bf2
turbo <joeyturbo@gmail.com>
2022-10-19 16:06:38 -0400

    converted app object to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hi</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <p>Hi</p>
+      </div>
+    )
+  }
 }
 
 export default App;
