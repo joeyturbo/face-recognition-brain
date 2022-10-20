09b5475ed775c49962b71abcb6849a2109f0d0e7
turbo <joeyturbo@gmail.com>
2022-10-20 10:06:29 -0400

    added functionality to record form input and detect button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ import './App.css';
 import 'tachyons';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+    onInputChange = (event) => {
+      console.log(event.target.value);
+  }
+
+    onButtonSubmit = () => {
+      console.log('click');
+    }
+
   render() {
     return(
       <div className="App">
@@ -15,7 +29,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}/>
         {/* <FaceRecognition /> */}
       </div>
     )
