71b8b61ebf103f8c5e8d762b4c5f80167eac3fb7
turbo <joeyturbo@gmail.com>
2022-10-20 09:17:35 -0400

    built navigation piece and applied gradient background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Navigation from './Components/Navigation/Navigation'
 import './App.css';
+import 'tachyons';
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-        {/* <Navigation />
-        <Logo />
+        <Navigation />
+        {/* <Logo />
         <ImageLinkForm />
         <FaceRecognition /> */}
       </div>
